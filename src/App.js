a2a4fa3136eea265d77b0fd17a3686fee2800360
don <don@don.com>
2022-11-20 18:20:18 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <p>
+        In this series we are going to learn building entire dashboard using React & Material design & vuexy
+      </p>
+
     </div>
   );
 }
 
 export default App;
+
+
